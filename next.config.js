ab983e70b67d8b59ffb37ae2a26dd370a92928ac
manuelmartin-developer <mmartindj@gmail.com>
2023-07-28 13:09:53 +0200

    fix: env variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  env: {
+    NEXT_PUBLIC_OPENAI_API_KEY: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+    NEXT_PUBLIC_ELEVENLABS_API_KEY: process.env.NEXT_PUBLIC_ELEVENLABS_API_KEY,
+    NEXT_PUBLIC_PRESENTER_VOICEID: process.env.NEXT_PUBLIC_PRESENTER_VOICEID
+  }
 };
 
 module.exports = nextConfig;
